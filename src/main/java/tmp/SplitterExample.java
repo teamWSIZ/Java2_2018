57277e7c7bdbff7591300a3f5ef920d570c2faf2
P Marecki <pmarecki.pro@gmail.com>
2018-04-07 14:43:21 +0200

    Przykład czytania strony www

--- src/main/java/tmp/SplitterExample.java
+++ src/main/java/tmp/SplitterExample.java
@@ -2,22 +2,44 @@ package tmp;
 
 import com.google.common.base.Splitter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class SplitterExample {
-    public static void main(String[] args) {
-        String w = "  a, b ,c,,,    , d";
+
+    static String readWebpage(String url) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        URL oracle = new URL(url);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            sb.append(inputLine);
+            sb.append('\n');
+        }
+        in.close();
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        String w = "a,c,b";
 
         Iterable<String> splitted =
-                Splitter.on(",").omitEmptyStrings().trimResults().split(w);
+                Splitter.on(",").trimResults().omitEmptyStrings().split(w);
 
         int x = 0;
         for(String s : splitted) {
+            System.out.println("Teraz s = " + s);
             x++;
         }
 
         System.out.println(splitted);
         System.out.println("Liczba słów:" + x);
 
+        System.out.println(readWebpage("https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html"));
+        
     }
 }
