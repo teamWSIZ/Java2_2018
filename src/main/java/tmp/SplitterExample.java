4b414b9dd8a8c67dcfc0075654abf711757d4d12
P Marecki <pmarecki.pro@gmail.com>
2018-04-07 10:51:11 +0200

    Sprawdzenie liczby słów

--- src/main/java/tmp/SplitterExample.java
+++ src/main/java/tmp/SplitterExample.java
@@ -2,6 +2,8 @@ package tmp;
 
 import com.google.common.base.Splitter;
 
+import java.util.ArrayList;
+
 public class SplitterExample {
     public static void main(String[] args) {
         String w = "  a, b ,c,,,    , d";
@@ -9,7 +11,13 @@ public class SplitterExample {
         Iterable<String> splitted =
                 Splitter.on(",").omitEmptyStrings().trimResults().split(w);
 
+        int x = 0;
+        for(String s : splitted) {
+            x++;
+        }
+
         System.out.println(splitted);
+        System.out.println("Liczba słów:" + x);
 
     }
 }
