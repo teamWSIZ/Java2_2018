b37049615e5bda44c84393902656eefdcf52cca9
P Marecki <pmarecki.pro@gmail.com>
2018-06-02 11:31:08 +0200

    pizza

---
+++ src/main/java/sets_and_inheritance/Pizza.java
@@ -0,0 +1,18 @@
+package sets_and_inheritance;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pizza {
+
+    public static void main(String[] args) {
+        List<String> porzebne = Arrays.asList("ciasto", "mozzarella", "salami",
+                "oregano");
+
+        List<String> dostepne = Arrays.asList("oregano", "mozzarella", "pieczarki"
+                , "boczek", "cebula");
+
+        //zadanie: wypisać których z "potrzebnych" składników narazie brakuje
+    }
+
+}
