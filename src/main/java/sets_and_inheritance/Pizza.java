f107dfb493a520e5adc8104ded809c8901bded15
P Marecki <pmarecki.pro@gmail.com>
2018-06-09 10:44:55 +0200

    pizza

--- src/main/java/sets_and_inheritance/Pizza.java
+++ src/main/java/sets_and_inheritance/Pizza.java
@@ -6,13 +6,27 @@ import java.util.List;
 public class Pizza {
 
     public static void main(String[] args) {
-        List<String> porzebne = Arrays.asList("ciasto", "mozzarella", "salami",
+        List<String> wymagane = Arrays.asList("ciasto", "mozzarella", "salami",
                 "oregano");
 
         List<String> dostepne = Arrays.asList("oregano", "mozzarella", "pieczarki"
                 , "boczek", "cebula");
 
         //zadanie: wypisać których z "potrzebnych" składników narazie brakuje
+
+        for(String s : wymagane) {
+            System.out.println("--> "  + s);
+            /// pytanie: czy "s" jest wewnątrz "dostępne" ???
+            int x = 0;
+            for(String r : dostepne) {
+                if (s.equals(r)) {
+                    x++;
+                }
+            }
+            if (x==0) {
+                System.out.println("brakuje: "  + s);
+            }
+        }
     }
 
 }
