dbe0b55ce268490cd7fef7e5c7510bf7ee005ee8
P Marecki <pmarecki.pro@gmail.com>
2018-06-30 10:23:02 +0200

    takie tam rozwiązanie template

---
+++ src/main/java/Rozw0.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Rozw0 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int x = s.nextInt();
+//        String ss = s.next();
+
+        ///
+
+        System.out.println(x * x);
+
+    }
+}
